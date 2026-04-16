e06f660eada612bc55055a45da2e891e24b73de0
SeinopSys <david@seinopsys.dev>
2026-04-16 19:30:22 +0200

    Add PWA vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-	build: {
-		outDir: 'dist',
-	},
-})
+  plugins: [VitePWA({ registerType: "autoUpdate" })],
+  build: {
+    outDir: "dist",
+  },
+});
